37985cc736da2066246ea844741824d3637afd29
OrdChaos <orderchaos@ordchaos.com>
2026-02-21 21:46:12 +0800

    github action init

--- ssg/include/Config.h
+++ ssg/include/Config.h
@@ -5,7 +5,7 @@
 
 namespace config {
     const double READ_TIME_UNITS_PER_MIN = 400.0;
-    const std::string SITE_URL = "https://www.ordchaos.com";
+    const std::string SITE_URL = "https://new.ordchaos.com";
     const std::string SITE_TITLE = "谐元场域";
     const std::string SITE_DESCRIPTION = "大抵是序炁的龙窝";
 }
